2ec10ee34c689ee4ed88128cacc5332415a8969d
namfohfalo <olaf@hofman.eu>
2023-12-10 18:08:19 +0100

    testcase

--- solver.py
+++ solver.py
@@ -12,6 +12,11 @@ zbior_t = None
 
 zbior_x, zbior_y, zbior_z, zbior_t = generator.generator(rozmiar_min, rozmiar_max, gestosc)
 
+# zbior_x = [0, 1, 2, 3]
+# zbior_y = [0, 1, 2]
+# zbior_z = [0, 1, 2, 3, 4]
+# zbior_t = [[1, 2, 3], [2, 2, 4], [0, 1, 3], [3, 2, 0], [1, 1, 3], [2, 0, 1]]
+
 rozwiazanie_zachlanne = zachlanny.greedy_3dm(zbior_x, zbior_y, zbior_z, zbior_t)
 rozwiazanie_potegowe = potegowy.power_set_3DM(zbior_t)
 
